b26d2ff909343fb5d59ca52130ea597007d0a538
jari <jari.bjorne@utu.fi>
2021-01-03 18:13:04 +0200

    Updated the interface of generate.py.

--- generate.py
+++ generate.py
@@ -1,20 +1,28 @@
+import os
 import md
 
-def generate(inPath, dataPath, scriptPath, outStem, outFormat, mode):
-    content = md.parse(inPath)
+def generate(inDir, exercises, outStem, outFormat, mode):
+    assert os.path.isdir(inDir)
+    content = []
+    for exercise in exercises:
+        content.append(md.parse(os.path.join(inDir, exercise)))
+        pyPath = os.path.join(inDir, exercise + ".py")
+        if os.path.exists(pyPath):
+            pass
+    
     options["mode"] = mode
     options["answers"] = mode in ("answers", "solutions")
-    options["fileStem"] = os.path.join(outStem)
+    options["fileStem"] = outStem
     md.renderDoc(content, options)
 
 if __name__=="__main__":
     from optparse import OptionParser
     optparser = OptionParser()
-    optparser.add_option("-i", "--input", default=None, help="The input file in Markdown format")
-    optparser.add_option("-d", "--data", default=None, help="A data file in JSON format")
+    optparser.add_option("-i", "--input", default=None, help="The input directory")
+    optparser.add_option("-e", "--exercises", default=None, help="Exercise names to include")
     optparser.add_option("-o", "--output", default=None, help="The output file stem")
     optparser.add_option("-f", "--format", default="latex")
     optparser.add_option("-m", "--mode", default="solutions", help="'questions', 'answers' or 'solutions'")
     (options, args) = optparser.parse_args()
 
-    generate()
+    generate(options.input, options.exercises, options.output, options.format, options.mode)
